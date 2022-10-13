f643bedc2c576f78a16ababf9ac8a339ff5350d6
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-12 19:52:24 -0700

    Layout del Journal Page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,17 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+        doloremque explicabo assumenda facere excepturi deleniti, ullam beatae,
+        labore molestias accusamus laborum est obcaecati error iusto. Totam ea
+        deleniti distinctio in.
+      </Typography>
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   );
 };
