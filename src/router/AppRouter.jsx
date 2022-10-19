1853a123dfb589c86c5936751711f1ccfb3f0309
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-19 15:41:50 -0700

    Mantener usuario activo al recargar pagina

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,27 @@
-import { useSelector } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { FirebaseAuth } from '../firebase/config';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { login, logout } from '../store/auth';
 import { CheckingAuth } from '../ui/';
 
 export const AppRouter = () => {
   const { status } = useSelector((state) => state.auth);
-  if(status === 'checking'){
-    return <CheckingAuth/>
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
+
+  if (status === 'checking') {
+    return <CheckingAuth />;
   }
   return (
     <Routes>
